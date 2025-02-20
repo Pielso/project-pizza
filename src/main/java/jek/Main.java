e5e0fa120d47d243f7ae0d7c53422fa1c4fe3019
Johan Ek <logist@live.se>
2025-02-20 15:46:43 +0100

    Created my very own hibernate-create-drop!

--- src/main/java/jek/Main.java
+++ src/main/java/jek/Main.java
@@ -6,31 +6,22 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-
+    public static void main(String[] args) throws Exception {
 
         if (DatabaseManager.getConnection().isValid(10)){
             System.out.println("Tja");
 
-
             Connection linkToDb = DatabaseManager.getConnection();
-            String insertIntoGames = "INSERT INTO games (title, min_players, max_players, rating) VALUES (?, ?, ?, ?);";
+            // String insertIntoGames = "INSERT INTO games (title, min_players, max_players, rating) VALUES (?, ?, ?, ?);";
 
             try {
                 DatabaseManager.dropDatabase();
                 DatabaseManager.createDatabase();
-//               PreparedStatement sqlIN = linkToDb.prepareStatement(insertIntoGames);
-//               sqlIN.setString(1, "1830");
-//               sqlIN.setInt(2, 3);
-//               sqlIN.setInt(3, 6);
-//               sqlIN.setInt(4, 10);
-//               sqlIN.execute();
-//               sqlIN.close();
-//               linkToDb.close();
-
-            } catch (SQLException e) {
+            }
+            catch (SQLException e) {
                 e.printStackTrace();
             }
         }
+
     }
 }
